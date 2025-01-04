dfc8ade157b1f5c2dada0b041991ef47bb58967c
dyomin7499 <dyomin7499@gmail.com>
2025-01-04 02:08:30 +0200

    project has been created

---
+++ ViewVault/ViewVault/ViewVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewVaultApp.swift
+//  ViewVault
+//
+//  Created by Exi on 04.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ViewVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViewVault/ViewVaultTests/ViewVaultTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewVaultTests.swift
+//  ViewVaultTests
+//
+//  Created by Exi on 04.01.2025.
+//
+
+import Testing
+@testable import ViewVault
+
+struct ViewVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
